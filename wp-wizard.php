887a8218d3c519ef8bbbe79bdbf97c1f523815cb
nimrod-cohen <nimrod.cohen@gmail.com>
2022-01-11 18:56:53 +0200

    rely on wpjsutils

--- wp-wizard.php
+++ wp-wizard.php
@@ -36,9 +36,7 @@ class WPWizard {
 
   function enqueueAssets()
 	{
-		wp_register_script("jsutils-js",plugin_dir_url(__FILE__).DIRECTORY_SEPARATOR."js".DIRECTORY_SEPARATOR."jsutils.js",[]);
-		wp_register_script("wpwizard-js",plugin_dir_url(__FILE__).DIRECTORY_SEPARATOR."js".DIRECTORY_SEPARATOR."wpwizard.js",['jsutils-js']);
-		wp_enqueue_script("jsutils-js");
+		wp_register_script("wpwizard-js",plugin_dir_url(__FILE__).DIRECTORY_SEPARATOR."js".DIRECTORY_SEPARATOR."wpwizard.js",['wpjsutils']);
     wp_enqueue_script("wpwizard-js");
 
     wp_register_style("wpwizard-style",plugin_dir_url(__FILE__).DIRECTORY_SEPARATOR."css".DIRECTORY_SEPARATOR."wpwizard.css");
